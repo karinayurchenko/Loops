e9c642d3630c0f8b513963fff70de69f8c173fd8
karinayurchenko <56835216+karinayurchenko@users.noreply.github.com>
2020-02-08 16:27:40 +0200

    new commit

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 
-let firstNumber = prompt (' Could you please enter the firts number ?', 10 );
+let firstNumber = Number(prompt (' Could you please enter the firts number ?', 10 ));
 while (isNaN(firstNumber)) {
-   firstNumber = prompt (' Error: You entered a string, please enter a number', 10 );
+   firstNumber = Number (prompt (' Error: You entered a string, please enter a number', 10 ));
 }
 
-let secondNumber = prompt (' Could you please enter the second number ?', 100 );
+let secondNumber = Number (prompt (' Could you please enter the second number ?', 100 ));
 while (isNaN(secondNumber)) {
-    secondNumber = prompt (' Error: You entered a string, please enter a number', 100 );
+    secondNumber = Number (prompt (' Error: You entered a string, please enter a number', 100 ));
  }
 
 const skip = confirm ('Is it necessary to skip  even numbers?');
-skip?temp = 'skip':temp = 'do not skip';
+const temp = skip? 'skip' : 'do not skip';
 
 let sum = 0;
-for (let i = firstNumber; i <= secondNumber; i++){  
-    if(skip){
-        if(i%2===0){
-           continue;
-        } 
+for (let i = firstNumber; i <= secondNumber; i++){
+    if(skip) {
+        if (i%2 === 0) {
+               continue;
+            } 
+        }
+        sum+=i;
+    console.log(sum);
     }
-    else{
-        sum += i;  
-    }  
-    console.log(i, sum);
-}
-document.writeln(`<p>First number: ${firstNumber}`);
-document.writeln(`<p>Second number: ${secondNumber}`);
-document.writeln(`<p> Skip even numbers: ${skip} <p>`);
 
-skip ? document.writeln(`Total amount beetween even  ${firstNumber} and ${secondNumber} is: ${sum}`) : document.writeln(`Total amount ${firstNumber} and ${secondNumber} is: ${sum}`);
+document.writeln(`<p>First number: ${firstNumber} `);
+document.writeln(`<p>Second number: ${secondNumber} <p>`);
+document.writeln(`Total amount beetween even  ${firstNumber} and ${secondNumber} is: ${sum}`);
